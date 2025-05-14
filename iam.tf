2ea9f846524752bc7e508b0e000c2bb6e083e253
Ashutosh Kumar <ashutosh@MacBookAir.lan>
2025-05-14 17:49:57 +0530

    push

--- iam.tf
+++ iam.tf
@@ -4,3 +4,10 @@ resource "google_project_iam_member" "project" {
   for_each = var.editor_member
   member  = "user:${each.value}"
 }
+
+resource "google_project_iam_binding" "editor" {
+  project = "your-project-id"
+  role    = "roles/editor"
+
+  members = var.editor_member
+}
